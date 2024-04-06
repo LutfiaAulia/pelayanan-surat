00368bd0d50cc3055f11886fee16e9413c09c885
septiamanda <septiaamanda592@gmail.com>
2024-04-06 14:03:02 +0700

    ....

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -115,6 +115,14 @@
                         </li>
                         
                         <li class="sidebar-title">Logout</li>
+
+                        <!-- Divider -->
+                        <hr class="sidebar-divider d-none d-md-block">
+
+                        <!-- Sidebar Toggler (Sidebar) -->
+                        <div class="text-center d-none d-md-inline">
+                            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+                        </div>
                         
                     </ul>
                 </div>
