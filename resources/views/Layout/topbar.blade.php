39b61524fd9480fedc5c4d8184732e4d2d242329
septiamanda <septiaamanda592@gmail.com>
2024-05-11 01:10:30 +0700

    upupup

---
+++ resources/views/Layout/topbar.blade.php
@@ -0,0 +1,21 @@
+{{-- Header --}}
+<header class="mb-3 d-flex justify-content-end align-items-center">
+    <div class="me-auto">
+        <h3>Dashboard</h3>
+    </div>
+    <div class="col-12 col-lg-3">
+        <div class="card">
+            <div class="card-body card-body-small py-3 px-4"> <!-- Tambahkan kelas card-body-small -->
+                <div class="d-flex align-items-center">
+                    <div class="avatar avatar-md"> <!-- Ubah ukuran avatar menjadi avatar-md -->
+                        <img src="{{ asset('template/assets/images/faces/1.jpg') }}" alt="Face 1">
+                    </div>
+                    <div class="ms-3 name">
+                        <h5 class="font-bold">Admin</h5>
+                        <h6 class="text-muted mb-0">admin@gmail.com</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
