af6744f521f9a6ed9bfc4255278f66cd2d00365a
septiamanda <septiaamanda592@gmail.com>
2024-05-17 15:24:00 +0700

    up

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -76,6 +76,7 @@
                     </div>
                 </div>
             </div>
+            
             <div class="row">
                 <div class="col-12">
                     <div class="card">
