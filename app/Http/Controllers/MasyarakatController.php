16ceddad67cfcbf6d19506c0235bb4b358d8b723
LutfiaAulia <lutfiaaulia2@gmail.com>
2024-06-13 18:06:31 +0700

    :)

--- app/Http/Controllers/MasyarakatController.php
+++ app/Http/Controllers/MasyarakatController.php
@@ -2,10 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\POT;
-use App\Models\Pengajuan;
-use App\Models\SKTM;
-use App\Models\SKU;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
