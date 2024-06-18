8048de384ce165f690c6b032e378892aa820f865
septiamanda <2111521008_septia@student.unand.ac.id>
2024-06-19 00:11:37 +0700

    h

--- resources/views/Masyarakat/syarpeng.blade.php
+++ resources/views/Masyarakat/syarpeng.blade.php
@@ -12,37 +12,60 @@
     </div>
     <section class="section">
         <div class="row">
-            <div class="col-12">
-                <div class="card">
+            <div class="col-12 col-md-6">
+                <div class="card mb-3">
                     <div class="card-header">
-                        <h5 class="card-title">Berikut Syarat Pengajuan Surat</h5>
+                        <h5 class="card-title">Syarat Pengajuan Surat</h5>
                     </div>
                     <div class="card-body">
-                        <p>
-                            Syarat Pengajuan Surat:
+                        <ul>
                             <li>Masyarakat harus memiliki identitas yang valid, seperti KTP</li>
                             <li>Tergantung pada jenis surat, pengguna mungkin perlu menyediakan data pendukung.</li>
                             <li>Masyarakat menyediakan keterangan tambahan yang diperlukan oleh wali nagari.</li>
                             <li>Masyarakat harus menyetujui kebijakan privasi dan persyaratan penggunaan aplikasi.</li>
-                        </p>
-                        <p>
-                            Syarat Pengajuan Surat SKTM:
-                            <li>Melampirkan KK</li>
-                            <li>Melampirkan KTP</li>
-                        </p>
-                        <p>
-                            Syarat Pengajuan Surat Penghasilan Orang Tua:
-                            <li>Melampirkan KK</li>
-                            <li>Memberikan keterangan penghasilan orang tua</li>
-                        </p>
-                        <p>
-                            Syarat Pengajuan SKU:
-                            <li>Melampirkan KTP terkait</li>
-                            <li>Melampirkan foto usaha yang dijalankan</li>
-                        </p>
+                        </ul>
+                        <p>Dengan memenuhi semua syarat di samping, diharapkan pengajuan surat dapat diproses dengan lancar dan sesuai dengan ketentuan yang berlaku.</p>
                     </div>
                 </div>
             </div>
+            <div class="col-12 col-md-6">
+                <div class="card mb-3">
+                    <div class="card-header">
+                        <h5 class="card-title">Syarat Pengajuan Surat SKTM</h5>
+                    </div>
+                    <div class="card-body">
+                        <ul>
+                            <li>Melampirkan KK</li>
+                            <li>Melampirkan KTP</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="card-header">
+                        <h5 class="card-title">Syarat Pengajuan Surat Penghasilan Orang Tua</h5>
+                    </div>
+                    <div class="card-body">
+                        <ul>
+                            <li>Melampirkan KK</li>
+                            <li>Memberikan keterangan penghasilan orang tua</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="card-header">
+                        <h5 class="card-title">Syarat Pengajuan SKU</h5>
+                    </div>
+                    <div class="card-body">
+                        <ul>
+                            <li>Melampirkan KTP terkait</li>
+                            <li>Melampirkan foto usaha yang dijalankan</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
+                
+            
         </div>
     </section>
 </div>
