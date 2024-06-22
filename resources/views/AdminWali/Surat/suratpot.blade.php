4b39ff94dd1c767af44aea53d8cf6cef4c7c3443
septiamanda <2111521008_septia@student.unand.ac.id>
2024-06-22 16:28:50 +0700

    ii

--- resources/views/AdminWali/Surat/suratpot.blade.php
+++ resources/views/AdminWali/Surat/suratpot.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Surat Keterangan Tidak Mampu</title>
+    <title>Surat Keterangan Usaha</title>
     <style>
         body {
             font-family: "Times New Roman", Times, serif;
