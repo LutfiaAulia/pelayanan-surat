bf36fa765fe62730ff14e969099e77302bee3764
septiamanda <2111521008_septia@student.unand.ac.id>
2024-06-23 16:15:27 +0700

    ...

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,14 @@
 
 @section('content')
 
-<body style="background-color: #aabeb5; background-size: cover;">
+<body>
     {{-- Landing Page --}}
     <div class="landing-page">
         <div class="container">
-            <div class="col-md-8 offset-md-2 text-center">
-                <h1 style="color: rgb(31, 63, 41);">Selamat Datang di Pelayanan Surat Menyurat Kantor Wali Nagari Pungguang Kasiak</h1>
+            <div class="row">
+                <div class="col-md-8 offset-md-2 text-center">
+                    <h1>Selamat Datang di Pelayanan Surat Menyurat Kantor Wali Nagari Pungguang Kasiak</h1>
+                </div>
             </div>
         </div>
     </div>
